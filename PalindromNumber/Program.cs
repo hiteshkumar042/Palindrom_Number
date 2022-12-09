4dd3910ba6ad44a11f9c40846af0cbd08d739512
hiteshkumar794 <hiteshkumar794@gmail.com>
2022-12-09 19:23:05 +0530

    Reverse program added to reverse branch

---
+++ PalindromNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace PalindromNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number: ");
+            int input =Convert.ToInt32(Console.ReadLine());
+            Reverse.ReverseFunction(input);
+        }
+    }
+}
